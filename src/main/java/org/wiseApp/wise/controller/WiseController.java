b6bb08b83ea20b39aeed01b0b81713c495749554
w0nnie <kjw55523@gmail.com>
2023-04-06 12:15:05 +0900

    service로직 분리

--- src/main/java/org/wiseApp/wise/controller/WiseController.java
+++ src/main/java/org/wiseApp/wise/controller/WiseController.java
@@ -3,17 +3,15 @@ package org.wiseApp.wise.controller;
 import org.wiseApp.Container;
 import org.wiseApp.Rq;
 import org.wiseApp.wise.entity.Wise;
+import org.wiseApp.wise.service.WiseService;
 
 import java.util.*;
 
 public class WiseController {
 
-    private List<Wise> wiseList;
-    private int wiseCount;
-
+    private WiseService wiseService;
     public WiseController() {
-        this.wiseList = new ArrayList<>();
-        this.wiseCount = 0;
+        wiseService = new WiseService();
     }
 
     public void write() {
@@ -21,18 +19,19 @@ public class WiseController {
         String wiseName = Container.getScanner().nextLine().trim();
         System.out.print("작가 : ");
         String writer = Container.getScanner().nextLine().trim();
-        wiseCount++;
-        Wise wise = new Wise(wiseCount, wiseName, writer);
-        wiseList.add(wise);
-        System.out.println(wiseCount + "번 명언이 등록되었습니다.");
+
+        long id = wiseService.write(wiseName, writer);
+        System.out.println(id + "번 명언이 등록되었습니다.");
     }
 
     public void list() {
+        List<Wise> wiseList = wiseService.findAll();
+
         System.out.println("번호  /  작가  /  명언");
         System.out.println("--------------------------");
-        for (int i = wiseList.size() - 1; i >= 0; i--) { // 2회반복
-            System.out.printf("%d / %s / %s\n", wiseList.get(i).getId(), wiseList.get(i).getWriter(), wiseList.get(i).getWiseName());
 
+        for (int i = wiseList.size() - 1; i >= 0; i--) {
+            System.out.printf("%d / %s / %s\n", wiseList.get(i).getId(), wiseList.get(i).getWriter(), wiseList.get(i).getWiseName());
         }
     }
 
@@ -42,14 +41,16 @@ public class WiseController {
             System.out.println("삭제할 id를 입력하세요(정수)");
             return;
         }
-        for (Wise wise : wiseList) {
-            if (wise.getId() == id) {
-                wiseList.remove(wise);
-                System.out.printf("%d번이 삭제되었습니다. \n", id);
-                return;
-            }
+
+        Wise wise = wiseService.findById(id);
+        if (wise == null) {
+            System.out.printf("%d번 명언이 존재하지 않습니다.\n",id);
+            return;
         }
-        System.out.printf("%d번 명언이 존재하지 않습니다.\n",id);
+
+        wiseService.remove(wise);
+        System.out.printf("%d번이 삭제되었습니다. \n", id);
+
     }
 
     public void update(Rq rq) {
@@ -58,17 +59,19 @@ public class WiseController {
             System.out.println("수정할 id를 입력하세요(정수)");
             return;
         }
-        for (Wise wise : wiseList) {
-            if (wise.getId() == id) {
-                System.out.print("명언 : ");
-                String newName = Container.getScanner().nextLine().trim();
-                System.out.print("작가 : ");
-                String newWriter = Container.getScanner().nextLine().trim();
-                wise.wiseUpdate(newName, newWriter);
-                System.out.printf("%d번이 수정되었습니다. \n", id);
-                return;
-            }
+
+        Wise wise = wiseService.findById(id);
+        if (wise == null) {
+            System.out.printf("%d번 명언이 존재하지 않습니다.\n",id);
+            return;
         }
-        System.out.printf("%d번 명언이 존재하지 않습니다.\n",id);
+        System.out.print("명언 : ");
+        String newName = Container.getScanner().nextLine().trim();
+        System.out.print("작가 : ");
+        String newWriter = Container.getScanner().nextLine().trim();
+
+        wiseService.update(wise, newName, newWriter);
+
+        System.out.printf("%d번이 수정되었습니다. \n", id);
     }
 }
